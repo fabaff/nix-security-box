3f61d0c598909893318fea1fc4809b53dafebec6
Fabian Affolter <mail@fabian-affolter.ch>
2022-06-16 16:44:30 +0200

    Create more output files

--- generate-docs.py
+++ generate-docs.py
@@ -35,21 +35,71 @@ def collect_tools():
     return output
 
 
-def count_tools(data):
-    """Count the available tools."""
-    data = collect_tools()
+def list_tools(data):
+    """Add all tools to a single list."""
+    tools = data
     tools_list = []
 
-    for _, tools in data.items():
+    for _, tools in tools.items():
         tools_list = tools_list + tools
 
-    print("Available tools:", len([tool for tool in tools_list if not ('#' in tool)]))
+    return [tool for tool in tools_list if not ("#" in tool)]
 
 
-def create_overview():
+def count_tools(data):
+    """Count the available tools."""
+    print("Available tools:", len(list_tools(data)))
+
+
+def full_list(data):
+    """Create a page with all availabe packages."""
+    tools = list_tools(data)
+    tools.sort()
+
+    mdFile = MdUtils(file_name="docs/list.md", title="Full list")
+
+    mdFile.new_line("```text")
+    for tool in tools:
+        mdFile.new_line(tool)
+    mdFile.new_line("```")
+
+    mdFile.create_md_file()
+
+
+def full_nix(data):
+    """Create a nix file which contains all tools."""
+    tools = list_tools(data)
+    tools.sort()
+
+    with open("docs/nix-security-tool-box.nix", "w") as nix_file:
+        nix_file.write("# Full package set from Nix Security Tool Box\n\n")
+        nix_file.write("{ pkgs, ... }:\n\n")
+        nix_file.write("{\n")
+        nix_file.write("  environment.systemPackages = with pkgs; [\n")
+        for tool in tools:
+            nix_file.write(f"    {tool}\n")
+        nix_file.write("  ];\n")
+        nix_file.write("}\n")
+
+
+def full_shell(data):
+    """Create a nix file which contains all tools."""
+    tools = list_tools(data)
+    tools.sort()
+
+    with open("docs/nstb-shell.nix", "w") as nix_file:
+        nix_file.write("{ pkgs ? import <nixpkgs> {} }:\n\n")
+        nix_file.write("with pkgs;\n\n")
+        nix_file.write("mkShell {\n")
+        nix_file.write(" nativeBuildInputs = [\n")
+        for tool in tools:
+            nix_file.write(f"    {tool}\n")
+        nix_file.write("  ];\n")
+        nix_file.write("}\n")
+
+
+def create_overview(data):
     """Create tools overview."""
-    data = collect_tools()
-
     mdFile = MdUtils(file_name="docs/index.md", title="Tool overview")
 
     for category, tools in data.items():
@@ -64,6 +114,18 @@ def create_overview():
         mdFile.new_line()
 
     mdFile.create_md_file()
-    count_tools(data)
 
-create_overview()
+
+def main():
+    """Main part of the script."""
+    data = collect_tools()
+
+    create_overview(data)
+    count_tools(data)
+    full_list(data)
+    full_nix(data)
+    full_shell(data)
+
+
+if __name__ == "__main__":
+    main()
